d6e7280b9d3414bb8e564f3561756360e2551318
“Matt” <mhallrp@gmail.com>
2023-12-27 21:36:16 +0100

    routes added

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const quizRoutes = require('./routes/quiz');
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
-
 const jwt = require('jsonwebtoken');
-
 const app = express();
-
+const helmet = require('helmet');
 app.use(express.json());
-
+app.use(helmet());
 app.use(session({
-  secret: 'your_secret_key',
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: {
@@ -19,34 +17,30 @@ app.use(session({
     sameSite: 'strict',
   }
 }));
-
 app.use(cors({
   origin: "https://quiz.matt-hall.dev",
   credentials: true,
 }));
-
 app.use("/", function auth(req, res, next) {
-  var origin = req.get('origin');
+  const origin = req.get('origin');
   if (origin != "https://quiz.matt-hall.dev"){
-  return res.redirect("https://matt-hall.dev")
+  return res.status(403)
   } else {
     next()
   }
 })
-
 app.use("/user", userRoutes);
-
 app.use("/", function auth(req, res, next) {
   res.set('Cache-Control', 'no-store');
   if (req.session.authorization) {
     let token = req.session.authorization['accessToken'];
-    jwt.verify(token, 'access', (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (!err) {
         req.user = user;
         next();
       } else {
         console.log('JWT Verification Error:', err);
-        return res.send('Hello World!');
+        return res.send('Error');
       }
     });
   } else {
@@ -54,7 +48,5 @@ app.use("/", function auth(req, res, next) {
     return res.status(403).json({ message: "User not logged in" });
   }
 });
-
 app.use("/quiz", quizRoutes);
-
 app.listen(process.env.PORT);
