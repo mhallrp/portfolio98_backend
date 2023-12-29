40e3677ded90dbee028ca309bbe0be4d6045dbae
“Matt” <mhallrp@gmail.com>
2023-12-29 17:31:34 +0100

    api retry code

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const helmet = require(`helmet`)
 
 app.use(express.json());
 
-app.use(helmet());
+app.use(helmet({
+  referrerPolicy: { policy: "origin-when-cross-origin" }
+}));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
