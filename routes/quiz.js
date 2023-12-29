b74466fdf3516f7ff10cc7ae083f595767bee9d8
“Matt” <mhallrp@gmail.com>
2023-12-29 16:45:19 +0100

    updated routes

--- routes/quiz.js
+++ routes/quiz.js
@@ -9,6 +9,7 @@ router.get('/trivia', async (req, res) => {
         console.log("this is the response data " + response.data.results)
         res.json(response.data.results);
     } catch (error) {
+        console.log("this is the error")
         res.status(500).send(error.message);
     }
 });
