b444f81ee0f4b9dd977b713106acdf8cdb4ec334
“Matt” <mhallrp@gmail.com>
2023-12-29 16:50:40 +0100

    api retry code

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const maxRetries = 3;
+const retryDelay = 3000;
+
+async function fetchTrivia(categoryId, attempt = 1) {
+    try {
+        const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${categoryId}`);
+        return response;
+    } catch (error) {
+        if (attempt < maxRetries) {
+            console.log(`Attempt ${attempt} failed, retrying in ${retryDelay}ms...`);
+            await new Promise(resolve => setTimeout(resolve, retryDelay));
+            return fetchTrivia(categoryId, attempt + 1);
+        } else {
+            throw error;
+        }
+    }
+}
 
 router.get('/trivia', async (req, res) => {
     const categoryId = req.query.category;
     try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${categoryId}`);
-        console.log("this is the response data " + response.data.results)
+        const response = await fetchTrivia(categoryId);
         res.json(response.data.results);
     } catch (error) {
-        console.log("this is the error")
-        res.status(500).send(error.message);
+        console.error(`All attempts failed: ${error.message}`);
+        res.status(500).send("Failed to fetch trivia questions");
     }
 });
 
