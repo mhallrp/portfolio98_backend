def1ada0f332c76c98a8944b1528527955b58258
“Matt” <mhallrp@gmail.com>
2023-12-30 14:32:41 +0100

    sql added

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 let users = [];
 module.exports = (connection) => {
-
 
     router.post("/register", async (req, res) => {
         const { username, password } = req.body.user;
