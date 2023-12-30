e5ab11e86e7763e61591143ac98fdbed8c369fad
“Matt” <mhallrp@gmail.com>
2023-12-30 12:48:54 +0100

    sql added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const helmet = require(`helmet`)
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'mysql://root:DHDDhgBbD45b2GHabGChCAfgc-1gb23b@mysql.railway.internal:3306/railway',
+    host: 'mysql.railway.internal',
     user: 'root',
     password: 'DHDDhgBbD45b2GHabGChCAfgc-1gb23b',
     database: 'railway'
