d13e0705a2fcd7b91dc2369d42071f2a5d761e57
“Matt” <mhallrp@gmail.com>
2023-12-30 13:03:24 +0100

    sql added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jwt = require(`jsonwebtoken`)
 const app = express()
 const helmet = require(`helmet`)
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'viaduct.proxy.rlwy.net',
     user: 'root',
