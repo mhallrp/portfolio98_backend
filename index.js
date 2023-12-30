787c6b8a17ee36bd3a59c6e5252f120a185bf5f6
“Matt” <mhallrp@gmail.com>
2023-12-30 12:53:10 +0100

    sql added

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const app = express()
 const helmet = require(`helmet`)
 
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
-    host: 'mysql.railway.internal',
-    user: 'root',
-    password: 'DHDDhgBbD45b2GHabGChCAfgc-1gb23b',
-    database: 'railway'
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 connection.connect(err => {
   if (err) {
