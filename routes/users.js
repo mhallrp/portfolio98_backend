809e543df1eb60b33fa762b788697f7b23463095
“Matt” <mhallrp@gmail.com>
2023-12-30 15:40:18 +0100

    bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcrypt');
 
 module.exports = (connection) => {
 
