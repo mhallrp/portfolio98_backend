1c13148134fb54770af2ed068cbf9e5d967e6656
“Matt” <mhallrp@gmail.com>
2024-01-04 11:38:52 +0100

    jwt removed

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-const express = require(`express`)
-const cors = require (`cors`)
-const session = require(`express-session`)
-const quizRoutes = require(`./routes/quiz`)
-const helmet = require(`helmet`)
-const app = express()
+const express = require(`express`);
+const cors = require(`cors`);
+const session = require(`express-session`);
+const quizRoutes = require(`./routes/quiz`);
+const helmet = require(`helmet`);
+const app = express();
 
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  port: process.env.PGPORT
+  port: process.env.PGPORT,
 });
 
-pool.connect(err => {
+pool.connect((err) => {
   if (err) {
-    console.error('Error connecting to PostgreSQL Database:', err);
+    console.error("Error connecting to PostgreSQL Database:", err);
     return;
   }
-  console.log('Connected to PostgreSQL Database!');
+  console.log("Connected to PostgreSQL Database!");
 });
 
-const userRoutes = require('./routes/users')(pool)
+const userRoutes = require("./routes/users")(pool);
 
 app.use(express.json());
 
 app.use(helmet());
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  proxy: true,
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    sameSite: 'none',
-    secure:true,
-    maxAge: 300000
-  }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    proxy: true,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      maxAge: 300000,
+    },
+  })
+);
 
-app.use(cors({
-  origin: "https://quiz.matt-hall.dev",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "https://quiz.matt-hall.dev",
+    credentials: true,
+  })
+);
 
 app.use("/", function auth(req, res, next) {
-  const origin = req.get('origin');
+  const origin = req.get("origin");
   if (origin !== "https://quiz.matt-hall.dev") {
     return res.status(403).json({ error: "Forbidden origin" });
   } else {
@@ -61,19 +65,24 @@ app.use("/quiz", quizRoutes);
 
 app.use("/", async function auth(req, res) {
   if (req.session.user) {
-      const userId = req.session.user.id;
-      try {
-          const results = await pool.query('SELECT username, total_score FROM users WHERE id = $1', [userId]);
-          if (results.rowCount === 0) {
-              return res.status(404).json({ error: "User not found" });
-          }
-          const user = results.rows[0];
-          res.status(200).json({ username: user.username, score: user.total_score });
-      } catch (dbErr) {
-          return res.status(500).json({ error: "Database error" });
+    const userId = req.session.user.id;
+    try {
+      const results = await pool.query(
+        "SELECT username, total_score FROM users WHERE id = $1",
+        [userId]
+      );
+      if (results.rowCount === 0) {
+        return res.status(404).json({ error: "User not found" });
       }
+      const user = results.rows[0];
+      res
+        .status(200)
+        .json({ username: user.username, score: user.total_score });
+    } catch (dbErr) {
+      return res.status(500).json({ error: "Database error" });
+    }
   } else {
-      res.status(403).json({ error: "No active session" });
+    res.status(403).json({ error: "No active session" });
   }
 });
 
