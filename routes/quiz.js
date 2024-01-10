2bdc6cec69dfddbbd840b535d7732952921ae716
“Matt” <mhallrp@gmail.com>
2024-01-10 17:04:38 +0100

    Quiz Table Added

--- routes/quiz.js
+++ routes/quiz.js
@@ -6,31 +6,46 @@ const retryDelay = 3000;
 
 module.exports = (pool) => {
 
-async function fetchTrivia(categoryId, attempt = 1) {
-    try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${categoryId}`);
-        return response;
-    } catch (error) {
-        if (attempt < maxRetries) {
-            console.log(`Attempt ${attempt} failed, retrying in ${retryDelay}ms...`);
-            await new Promise(resolve => setTimeout(resolve, retryDelay));
-            return fetchTrivia(categoryId, attempt + 1);
-        } else {
-            throw error;
-        }
-    }
-}
+// async function fetchTrivia(categoryId, attempt = 1) {
+//     try {
+//         const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${categoryId}`);
+//         return response;
+//     } catch (error) {
+//         if (attempt < maxRetries) {
+//             console.log(`Attempt ${attempt} failed, retrying in ${retryDelay}ms...`);
+//             await new Promise(resolve => setTimeout(resolve, retryDelay));
+//             return fetchTrivia(categoryId, attempt + 1);
+//         } else {
+//             throw error;
+//         }
+//     }
+// }
 
 router.get('/trivia', async (req, res) => {
     const categoryId = req.query.category;
+
     try {
-        const response = await fetchTrivia(categoryId);
-        res.json(response.data.results);
-    } catch (error) {
-        console.error(`All attempts failed: ${error.message}`);
-        res.status(500).send("Failed to fetch trivia questions");
-    }
+        const query = `
+          SELECT * FROM your_table_name
+          WHERE category = 'Music'
+          ORDER BY RANDOM()
+          LIMIT 10;
+        `;
+    
+        const { rows } = await pool.query(query);
+        res.json(rows);
+      } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    // try {
+    //     const response = await fetchTrivia(categoryId);
+    //     res.json(response.data.results);
+    // } catch (error) {
+    //     console.error(`All attempts failed: ${error.message}`);
+    //     res.status(500).send("Failed to fetch trivia questions");
+    // }
 });
+
 router.get('/categories', async (req, res) => {
     try {
         const queryResult = await pool.query("SELECT DISTINCT category FROM quiz");
@@ -44,7 +59,6 @@ router.get('/categories', async (req, res) => {
         res.status(500).send("Failed to fetch categories");
     }
 });
-
 
 return router
 
