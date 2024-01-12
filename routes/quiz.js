9f678d162169be35ea78ad7bd5fe82ee449bf027
“Matt” <mhallrp@gmail.com>
2024-01-12 18:05:48 +0100

    rm reverese

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,47 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const maxRetries = 3;
-const retryDelay = 3000;
 
 module.exports = (pool) => {
-
-router.get('/trivia', async (req, res) => {
+  router.get("/trivia", async (req, res) => {
     const categoryId = req.query.category;
-
     try {
-
-        const queryResult = await pool.query("SELECT DISTINCT category FROM quiz");
-        const categories = queryResult.rows.map((row) => (row.category));
-        const category = categories[categoryId]
-        const query = `
+      const queryResult = await pool.query(
+        "SELECT DISTINCT category FROM quiz"
+      );
+      const categories = queryResult.rows.map((row) => row.category);
+      const category = categories.reverse()[categoryId];
+      const query = `
           SELECT * FROM quiz
           WHERE category = '${category}'
           ORDER BY RANDOM()
           LIMIT 10;
         `;
-    
-        const { rows } = await pool.query(query);
-        res.json(rows);
-      } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-      }
-});
 
-router.get('/categories', async (req, res) => {
-    try {
-        const queryResult = await pool.query("SELECT DISTINCT category FROM quiz");
-        const categories = queryResult.rows.map((row, index) => ({
-            id: index,
-            name: row.category
-        }));
-        res.json(categories.reverse());
+      const { rows } = await pool.query(query);
+      res.json(rows);
     } catch (error) {
-        console.error("Error fetching categories:", error);
-        res.status(500).send("Failed to fetch categories");
+      res.status(500).json({ error: "Internal server error" });
     }
-});
+  });
 
-return router
+  router.get("/categories", async (req, res) => {
+    try {
+      const queryResult = await pool.query(
+        "SELECT DISTINCT category FROM quiz"
+      );
+      const categories = queryResult.rows.map((row, index) => ({
+        id: index,
+        name: row.category,
+      }));
+      res.json(categories.reverse());
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+      res.status(500).send("Failed to fetch categories");
+    }
+  });
 
-}
+  return router;
+};
