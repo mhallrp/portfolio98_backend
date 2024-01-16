f0a0463887828ed1c5720e8bd9fd54a84566d4c3
“Matt” <mhallrp@gmail.com>
2024-01-16 22:56:17 +0100

    Error check added

--- routes/generate.js
+++ routes/generate.js
@@ -12,8 +12,7 @@ app.post("/generate-quiz", async (req, res) => {
       messages: [
         {
           role: "system",
-          content:
-            `Respond with a string that is delimited with the pipe character ensuring there are no spaces either side of the pipe character. The string should be made up of a question where the answer is ${topic} and 3 incorrect answers to that question (the question and all three of the answers should be delimited with the pipe character). Ensure that the answers are consistent with each i.e. similar wording so that the correct answer does not stand out among the incorrect answers. Ensure that the correct answer is always the last answer in the string.`,
+          content: `Respond with a string that is delimited with the pipe character ensuring there are no spaces either side of the pipe character. The string should be made up of a question where the answer is ${topic} and 3 incorrect answers to that question (the question and all three of the answers should be delimited with the pipe character). Ensure that the answers are consistent with each i.e. similar wording so that the correct answer does not stand out among the incorrect answers. Ensure that the correct answer is always the last answer in the string. If you don't recognise the key word return with a non | delimited string with the word error`,
         },
       ],
       model: "gpt-3.5-turbo",
