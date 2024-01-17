b85d446ae79018a436f9bd14e38e6bc9d29c91e0
“Matt” <mhallrp@gmail.com>
2024-01-17 23:51:11 +0100

    prompt updated

--- routes/generate.js
+++ routes/generate.js
@@ -12,17 +12,11 @@ app.post("/generate-quiz", async (req, res) => {
       messages: [
         {
           role: "system",
-          content: `Given the following string *start of string*"${answer}"*end of string* create a trivia question associated to the subject, base your response on the following context *start of context*"${topic}"*end of context*, return 3 relevant incorrect answers to go with the question. Return all 4 values as a comma delimited string, the format of the string should be:
-          
-          "[Question],[CorrectAnswer],[IncorrectAnswer1],[IncorrectAnswer2],[IncorrectAnswer3]" 
-          
-          Do not deviate from this single line format by adding any extra formatting, new lines or additional information or include the input string or context from this request in the response. The question should be relatively difficult. Follow the formatting outlined here explicitly.
-          
-          Ensure the the returned answers are faitly similar so that the correct answer does not stand out. Also ensure the answers are of the similar length and are no more than 10 words in length.`
+          content: `Given the following string *start of string*"${answer}"*end of string* create a trivia question associated to the subject, base your response on the following context *start of context*"${topic}"*end of context*, return 3 relevant incorrect answers to go with the question. Return all 4 values as a comma delimited string, the format of the string should be: "[Question],[CorrectAnswer],[IncorrectAnswer1],[IncorrectAnswer2],[IncorrectAnswer3]" Do not deviate from this single line format by adding any extra formatting, new lines or additional information or include the input string or context from this request in the response. The question should be relatively difficult. Follow the formatting outlined here explicitly. Ensure the the returned answers are faitly similar so that the correct answer does not stand out. Also ensure the answers are of the similar length and are no more than 10 words in length.`
           ,
         },
       ],
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-1106",
     });
 
     console.log(completion);
